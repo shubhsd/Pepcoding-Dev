aff104a0843b356454fbd555f7c90760b6881705
shubhsd <shubhamsd53@gmail.com>
2022-03-22 21:15:54 +0530

    array methods

---
+++ 2.Javascript/Lecture 11/arrays.js
@@ -0,0 +1,19 @@
+// arrays - collection of element  of any type
+
+let cars = ["BMW", "audi", "MG", 1,2,3,true];
+
+console.log(cars);
+
+// accessing elements of array
+
+console.log(cars[0]);
+
+// replacing elements in array
+
+cars[3] = "Mahindra";
+
+console.log(cars[3]);
+console.log(cars);
+
+cars[7] = "Tata"
+console.log(cars);
